9872ecea6909b70efbeeebe8e9fa531dcd3c1d9b
Allan Shrestha <stevevai111@hotmail.com>
2022-01-25 22:56:52 +0000

    Added colors to buttons and overall styling fixed

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,3 +1,4 @@
+"use strict";
 
 // const buttonZero = document.querySelector(".zeroZero");
 // const buttonOne = document.querySelector(".one");
@@ -16,118 +17,75 @@
 // const buttonDivide = document.querySelector(".devide");
 // const buttonPercentage = document.querySelector(".percentage");
 // const buttonBrackets = document.querySelector(".brackets");
-const buttonClear = document.querySelector(".clearAll");
-// const buttonPlusMinus = document.querySelector(".plusMinus")
+var buttonClear = document.querySelector(".clearAll"); // const buttonPlusMinus = document.querySelector(".plusMinus")
 
-const calcArr = [];
-const head = document.querySelector(".head");
-let equals = document.querySelector(".equals");
-let number = document.querySelectorAll(".num");
-let operator = document.querySelectorAll(".operator");
-
-let firstNum;
-let secondNum;
-
-number.forEach(item => {
-  item.addEventListener("click", event => {
-    head.innerHTML += item.innerHTML; 
-  })
-})
-
-operator.forEach(item => {
-  firstNum = head.innerHTML
-  item.addEventListener("click", event => {
+var calcArr = [];
+var head = document.querySelector(".head");
+var equals = document.querySelector(".equals");
+var number = document.querySelectorAll(".num");
+var operator = document.querySelectorAll(".operator");
+var firstNum;
+var secondNum;
+number.forEach(function (item) {
+  item.addEventListener("click", function (event) {
     head.innerHTML += item.innerHTML;
-  })
-})
+  });
+});
+operator.forEach(function (item) {
+  firstNum = head.innerHTML;
+  item.addEventListener("click", function (event) {
+    head.innerHTML += item.innerHTML;
+  });
+});
+equals.addEventListener("click", function (event) {
+  secondNum = head.innerHTML;
 
-equals.addEventListener("click", event => {
-  secondNum = head.innerHTML
-  if (operator == "+") { 
-    let result = number(firstNum) + number(secondNum);
+  if (operator == "+") {
+    var result = number(firstNum) + number(secondNum);
     head.innerHTML = "";
     head.innerHTML = result;
-  } 
-})
-
-
-
-
-
-
-
-
-
-
-
-// buttonZero.addEventListener("click", (event) => {
+  }
+}); // buttonZero.addEventListener("click", (event) => {
 //   head.innerHTML += (0)
 //   calcArr.push(0)
 // })
-
 // buttonOne.addEventListener("click", (event) => {
 //   head.innerHTML += (1)
 // })
-
 // buttonTwo.addEventListener("click", (event) => {
 //   head.innerHTML += (2)
 // })
-
 // buttonThree.addEventListener("click", (event) => {
 //   head.innerHTML += (3)
 // })
-
 // buttonFour.addEventListener("click", (event) => {
 //   head.innerHTML += (4)
 // })
-
 // buttonFive.addEventListener("click", (event) => {
 //   head.innerHTML += (5)
 // })
-
 // buttonSix.addEventListener("click", (event) => {
 //   head.innerHTML += (6)
 // })
-
 // buttonSeven.addEventListener("click", (event) => {
 //   head.innerHTML += (7)
 // })
-
 // buttonEight.addEventListener("click", (event) => {
 //   head.innerHTML += (8)
 // })
-
 // buttonNine.addEventListener("click", (event) => {
 //   head.innerHTML += (9)
 // })
-
 // buttonDot.addEventListener("click", (event) => {
 //   head.innerHTML += (".")
 // })
-
 // buttonEquals.addEventListener("click", (event) => {
 //   head.innerHTML = "";
 // })
-
 // buttonPlus.addEventListener("click", (event) => {
 //   head.innerHTML = "";
 // })
-buttonClear.addEventListener("click", (event) => {
+
+buttonClear.addEventListener("click", function (event) {
   head.innerHTML = "";
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
